753370a9c4310f163199cf8c4a3c1b9c708db9d2
Micweezy <35112101+Micweezy@users.noreply.github.com>
2020-10-04 16:22:48 +0100

    Second Commit

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -11,19 +11,19 @@ import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
 
-    public void createMapIntent() {
-        createMapIntent();
-        final Uri parse = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, final Uri);
-        Intent setPackage("com.google.android.apps.maps");
-        if(mapIntent.resolveActivity(getPackageManager()) != null);{
+    public void createMapIntent(View view) {
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager())!= null) {
             startActivity(mapIntent);
         }
 
 
+
     }
 
-    public void createMapIntent(View view)
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
